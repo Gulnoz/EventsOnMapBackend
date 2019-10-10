b66952deb27450059832ea5186dd29f1397f5aae
Gulnoza Muminova <gmcharmy@gmail.com>
2019-10-10 10:16:27 -0400

    db, models, controllers

---
+++ db/migrate/20191010140547_create_user_events.rb
@@ -0,0 +1,10 @@
+class CreateUserEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_events do |t|
+      t.belongs_to :user, foreign_key: true
+      t.integer :event_id
+
+      t.timestamps
+    end
+  end
+end
