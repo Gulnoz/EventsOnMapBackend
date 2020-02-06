9b7605e7e711bb76d2884a7f41ebb63c12d4b925
Gulnoza Muminova <gmcharmy@gmail.com>
2020-02-05 21:42:49 -0500

    Fixing jwt bug

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,7 @@
 class AuthController < ApplicationController
 MY_SECRET = ENV['my_secret']
 
+
 def login
     @user = User.find_by(email: login_params[:email])
     if @user && @user.authenticate(login_params[:password])
@@ -13,13 +14,17 @@ def login
     end
 end
 def persist
+    
     if request.headers['Authorization']
+        byebug
         encoded_token = request.headers['Authorization'].split(' ')[1]
         begin
             token = JWT.decode(encoded_token, MY_SECRET)
             user_id = token[0]['user_id']
             user = User.find(user_id)
-            render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+            token = JWT.encode({user_id: user.id}, MY_SECRET, 'HS256')
+       
+            render json: { user: UserSerializer.new(user), jwt: token }, status: :accepted
         rescue JWT::DecodeError
              render json: ({'error':'test'})
         end
